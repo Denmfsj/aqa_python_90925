2bed7d3a1c9e567ea960f18731ed272f53447cbe
Denmfsj <mfsj.p2@gmail.com>
2025-09-12 18:46:14 +0200

    added file from lesson 1

---
+++ main.py
@@ -0,0 +1,31 @@
+print("My name is Denys")
+
+# незмінні типи
+my_name = 'Denys'  # str, string, строка
+my_age = 34  # int, integer, число
+my_temp = 36.6  # float, число з плаваучою точккою
+am_i_exist = True  # boolena, bool логічне True or False
+None  # None, нічого
+my_tuple = ('some info', 42)  # tuple, кортеж
+
+# змінні типи
+my_friends = ['Alex', 'Olena']  # list, список
+my_info = {'name': 'Denys', 'age': 34}  # dict, словник
+my_numbers = {1, 2, 3, 4}  # set, множина
+
+print("My age is 35",  my_age == 35)  # False
+print("My age is 34", my_age == 34)  # True
+my_age == 34.0  # ?
+my_age == '34'  # ?
+
+# snake_case  - пишемо імена змінних і функцій, імена файлів і папок
+# CAPS_LOCK_WITH_UNDERSCORE - константи
+# MyClass - CamelCase - там пишуть імена клаксів
+
+
+for number in [1, 2, 3]:
+    print('Number is', number)
+print('------------')
+my__number = 42
+
+# ctrl(command) + alt + l
