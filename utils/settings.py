26df98aec46cd48f839a9040e6f5daf32da93408
den <medenis91@gmail.com>
2025-12-02 20:26:20 +0100

    fix for dynaconf

--- utils/settings.py
+++ utils/settings.py
@@ -5,10 +5,7 @@ from definitions import BASE_PATH
 # pip isntall dynaconf
 d_settings = Dynaconf(
     envvar_prefix="DYNACONF",
-    settings_files=[],#BASE_PATH / ".settings.ini", BASE_PATH / ".secrets.ini"],
+    settings_files=[BASE_PATH / ".settings.toml", BASE_PATH / ".secrets.toml"],
     environments=True,
     load_dotenv=True,
 )
-
-print(d_settings.STUDENT_URL)
-
